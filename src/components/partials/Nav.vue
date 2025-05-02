4c5f134320f2a7652a71f732e7f241a5df50795d
Deyan Altanov <deqn87@yahoo.com>
2025-05-02 13:48:47 +0000

    Add profile picture in the nav.

--- src/components/partials/Nav.vue
+++ src/components/partials/Nav.vue
@@ -3,10 +3,18 @@
         <router-link to="/dashboard">
             <img id='nav-logo' src="/nav.png" />
         </router-link>
-        <div v-if="authStore.user">
-            Welcome, {{ authStore.user.first_name }} {{ authStore.user.last_name }} |
-            <button @click="logout">Logout</button>
+
+        <div v-if="authStore.user" class="relative">
+            <button @click="toggleDropdown" id='nav_profile_img_btn' class="flex items-center gap-2 focus:outline-none">
+                <img id='nav_profile_img' :src="authStore.user.avatar_base64" />
+            </button>
+
+            <div v-if="dropdownOpen" class="absolute right-0 mt-2 w-40 bg-white border rounded shadow z-10">
+                <router-link to="/profile" class="block px-4 py-2 hover:bg-gray-100">Profile</router-link>
+                <button @click="logout" class="block w-full text-left px-4 py-2 hover:bg-gray-100">Logout</button>
+            </div>
         </div>
+
         <div v-else>
             <router-link to="/login">Login</router-link> |
             <router-link to="/register">Register</router-link>
@@ -16,11 +24,18 @@
 
 <script setup>
     import axios from 'axios'
+    import { ref } from 'vue'
     import { useAuthStore } from '../../stores/auth'
     import { useRouter } from 'vue-router'
 
     const authStore = useAuthStore()
     const router = useRouter()
+
+    const dropdownOpen = ref(false)
+
+    const toggleDropdown = () => {
+        dropdownOpen.value = !dropdownOpen.value
+    }
 
     const logout = async () => {
         try {
